7d6af472ccbc6f7c2d7a0c5cee4e0cf917b660db
Je <jeannela@foxmail.com>
2016-05-06 23:33:01 +0800

    Update: 2016/5/6
    => add file `Playlist3.js'

---
+++ themes/Mnlt2/scripts/Playlist3.js
@@ -0,0 +1,19 @@
+﻿// vim:set ft=javascript fileencoding=utf-8 bomb et:
+//
+// ==PREPROCESSOR==
+// @feature "dragdrop"
+// @author "elia_is_me"
+// @import "%fb2k_profile_path%skins\Mnlt2\common\common4.js"
+// ==/PREPROCESSOR==
+//
+// Require:
+// -> foobar2000 v1.3.3+ (required by wshmp)
+// -> WSH Panel Mod Plus 1.5.7+
+//
+// Update: 2016-05-06
+//
+// 技术验证用，请勿用于日常使用。
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
